17870271566425474eb867d450a1598b0e78d77b
Tiberiu-Biking <tiberiu.popa@gmail.com>
2016-11-18 23:27:05 +0100

    #7 - banner added, jpa properties set

--- dashboard-app/src/main/java/com/est/batch/dashboard/DashboardApplication.java
+++ dashboard-app/src/main/java/com/est/batch/dashboard/DashboardApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class DashboardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
+        SpringApplication.run(DashboardApplication.class, args);
     }
 }
