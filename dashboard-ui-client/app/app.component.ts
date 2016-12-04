b7aac6905fd93c7e5c82b7033101181cdb536bec
Slawek <sziolkow@gmail.com>
2016-12-04 15:53:05 +0100

    making the navigation a  bit prettier

--- dashboard-ui-client/app/app.component.ts
+++ dashboard-ui-client/app/app.component.ts
@@ -5,11 +5,21 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/home" routerLinkActive="active">Home</a>
-      <a routerLink="/jobs" routerLinkActive="active">Jobs</a>
-      <a routerLink="/executions" routerLinkActive="active">Executions</a>
-      <a routerLink="/files" routerLinkActive="active">Files</a>
+    <nav class="navbar navbar-default navbar-static-top">
+      <ul class="nav navbar-nav">
+        <li class="active">
+          <a routerLink="/home" routerLinkActive="active">Home</a>
+        </li>
+        <li>
+          <a routerLink="/jobs" routerLinkActive="active">Jobs</a>
+        </li>
+        <li>
+          <a routerLink="/executions" routerLinkActive="active">Executions</a>
+        </li>
+        <li>
+          <a routerLink="/files" routerLinkActive="active">Files</a>
+        </li>
+      </ul>
     </nav>
     <router-outlet></router-outlet>
   `,
